f8a255195800a73a4dcbb815cf8b18e47e6bba71
Celebro01 <dangerveneta@gmail.com>
2021-02-23 14:21:52 -0300

    Adicionando primeira pagina

--- App/Views/index/home.php
+++ App/Views/index/home.php
@@ -1,11 +1,361 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pagina</title>
-</head>
-<body>
-    <h1>Pagina inicial</h1>
-</body>
-</html>
+<link rel="stylesheet" href="src/css/partes/main.css">
+<div class="banner">
+</div>
+<main>
+    <div class="area-principal">
+        <div class="selectors">
+            <div class="selector">
+                Subbed
+            </div>
+            <div class="selector">
+                Dubbed
+            </div>
+        </div>
+        <!--Animes-->
+        <div class="area-animes">
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://cabanadoleitor.com.br/wp-content/uploads/2020/05/attack-on-titan-shingeki-no-kyojin-final-season-anime_CDL_1280x720_01.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+            
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://www.jbox.com.br/wp/wp-content/uploads/2020/06/onepiece-destacada.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://ovicio.com.br/wp-content/uploads/2020/10/20201030-jujutsu-1200x675.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://cabanadoleitor.com.br/wp-content/uploads/2020/05/attack-on-titan-shingeki-no-kyojin-final-season-anime_CDL_1280x720_01.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://cabanadoleitor.com.br/wp-content/uploads/2020/05/attack-on-titan-shingeki-no-kyojin-final-season-anime_CDL_1280x720_01.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://cabanadoleitor.com.br/wp-content/uploads/2020/05/attack-on-titan-shingeki-no-kyojin-final-season-anime_CDL_1280x720_01.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://cabanadoleitor.com.br/wp-content/uploads/2020/05/attack-on-titan-shingeki-no-kyojin-final-season-anime_CDL_1280x720_01.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://cabanadoleitor.com.br/wp-content/uploads/2020/05/attack-on-titan-shingeki-no-kyojin-final-season-anime_CDL_1280x720_01.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://cabanadoleitor.com.br/wp-content/uploads/2020/05/attack-on-titan-shingeki-no-kyojin-final-season-anime_CDL_1280x720_01.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://cabanadoleitor.com.br/wp-content/uploads/2020/05/attack-on-titan-shingeki-no-kyojin-final-season-anime_CDL_1280x720_01.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://cabanadoleitor.com.br/wp-content/uploads/2020/05/attack-on-titan-shingeki-no-kyojin-final-season-anime_CDL_1280x720_01.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+
+
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://cabanadoleitor.com.br/wp-content/uploads/2020/05/attack-on-titan-shingeki-no-kyojin-final-season-anime_CDL_1280x720_01.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+
+
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://cabanadoleitor.com.br/wp-content/uploads/2020/05/attack-on-titan-shingeki-no-kyojin-final-season-anime_CDL_1280x720_01.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://cabanadoleitor.com.br/wp-content/uploads/2020/05/attack-on-titan-shingeki-no-kyojin-final-season-anime_CDL_1280x720_01.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+
+
+            <div class="anime">
+                <div class="cabecalho">
+                    <img src="https://cabanadoleitor.com.br/wp-content/uploads/2020/05/attack-on-titan-shingeki-no-kyojin-final-season-anime_CDL_1280x720_01.jpg" alt="" width="100%">
+                </div>
+                <div class="descricao">
+                    <p>Episode 3</p>
+                    <p>4 h ago</p>
+                </div>
+                <div class="footer">
+                    Shingeki no Kyojin
+
+                </div>
+            </div>
+
+
+        </div> <!--Fim area dos animes-->
+        <div class="footer">
+            <div class="selector next">
+                Next Page
+            </div>
+        </div>
+    </div>
+
+    <div class="area-lateral">
+        <div class="noticia">
+            <div class="cabecalho">
+                Join Our Discord
+            </div>
+            <div class="body">
+                <span class="mensagem"> Chat with 2300+ awasome weebs;</span>
+                <button>Join The Discord</button>
+            </div>
+        </div>
+
+        <div class="noticia">
+            <div class="cabecalho">
+                Join Us on Regedit
+            </div>
+            <div class="body">
+                <span  class="mensagem">Ask and share memes</span>
+                <button>Join The Regedit</button>
+            </div>
+        </div>
+        <!--Lista dos animes ainda sendo postados-->
+        <div class="noticia anime-nome-list">
+            <div class="cabecalho">
+                Ongoing Animes
+            </div>
+            <div class="nomes-container">
+                <div class="body">
+                    <span  class="mensagem">Shingeki no Kyojin Shingeki </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Boruto</span>
+                </div> 
+                <div class="body">
+                    <span  class="mensagem">Kimetsu no Yaiba </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Jujutsu Kaisen</span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Horimiya </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+                <div class="body">
+                    <span  class="mensagem">Mushuko Tensei </span>
+                </div>
+            </div>
+        </div>
+    </div> 
+</main>
--- App/Views/layout.php
+++ App/Views/layout.php
@@ -3,10 +3,59 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meu layout</title>
+    <title>Anime</title>
+    <!--Fontes-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=DotGothic16&display=swap" rel="stylesheet">
+    <!--Css-->
+    <link rel="stylesheet" href="src/css/normalize.css">
+    <link rel="stylesheet" href="src/css/style.css">
+    <link rel="stylesheet" href="src/css/partes/header.css">
+    <link rel="stylesheet" href="src/css/partes/scrollbar.css">
+    <link rel="stylesheet" href="src/css/partes/footer.css">
+
+    <!--Favicon-->
+    <link rel='icon' href='src/fotos/favicon.ico' type='image/x-icon'>
 </head>
 <body>
-    <h2>Esse é um layout</h2>
+    <header>
+        <nav class="navbar">
+            <div class="left">
+                <a href="" class="brand"></a>
+                <div class="area-barra-pesquisar">
+                    <form action="">
+                        <input type="text" class="barra-pesquisar" placeholder="Search">
+                        <input type="submit" class="btn-pesquisar" value="" >
+                    </form>
+                </div>
+            </div>
+
+            <ul class="nav">
+                <li><a href="">Most Popular</a></li>
+                <li><a href="">Anime</a></li>
+                <li><a href="">Dubbed</a></li>
+                <li><a href="">Movies</a></li>
+                <li><a href="">Categories</a></li>
+                <li><a href="">Schedule</a></li>
+                <li><a href="">Random</a></li>
+            </ul>
+        </nav>
+    </header>
     <?php $this->content(); ?>
+    <footer>
+        <div class="content">
+            <div class="area-links">
+                <p>Join us:</p>
+                <div class="links">
+                    <button>Discod</button>
+                    <button>Regedit</button>
+                </div>
+            </div>
+            <div class="">
+                DMCA - Privacy Policy - AnimeKisa.org - AnimeKisa.net
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
